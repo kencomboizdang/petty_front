23cdab9361b204acb578f413834227ab51973ea3
kencomboizdang <kencomboizdang@gmail.com>
2020-07-25 22:17:19 +0700

    Update index.js

--- app/containers/OrderDetailContainer/index.js
+++ app/containers/OrderDetailContainer/index.js
@@ -38,183 +38,202 @@ class OrderDetailContainer extends React.Component {
   render() {
     return (
       <div>
-        <div
-          className="Products"
-          style={{
-            width: '658px',
-
-            boxSizing: 'border-box',
-            border: '1px solid gray',
-            boxShadow: '0px 0px 0px 1px rgba(0,0,0,0.3)',
-            borderRadius: '5px',
-            float: 'left',
-          }}
-        >
-          <div class="d-flex flex-row bd-highlight mb-3">
-            <div class="p-2 bd-highlight">
-              {' '}
-              <img
-                src="https://cf.shopee.vn/file/f82f8ccb649afcdf4f07f1dd9c41bcb0"
-                class="sidebar-menu-item-icon"
-                style={{ width: '15px', height: '15px' }}
-              />
-            </div>
-            <div class="p-2 bd-highlight">
-              <span style={{ marginBottom: '10px' }}>QUẢN LÝ ĐƠN HÀNG</span>
-            </div>
-          </div>
-          <div
-            class="d-flex flex-row bd-highlight mb-3"
-            style={{ boxSizing: 'border-box' }}
-          >
-            <div class="p-2 bd-highlight">
-              {' '}
-              <InputComponent text={'Tìm sản phẩm'} />
-            </div>
-            <div class="p-2 bd-highlight">
-              <Button type="primary">Tìm theo QR</Button>
-            </div>
-          </div>
-          <TableComponent />
-        </div>
-        <div
-          className="Customers"
-          style={{
-            width: '600px',
-
-            boxSizing: 'border-box',
-            border: '1px solid gray',
-            boxShadow: '0px 0px 0px 1px rgba(0,0,0,0.3)',
-            borderRadius: '5px',
-            float: 'right',
-          }}
-        >
-          <div class="d-flex flex-column bd-highlight mb-3">
-            <div class="p-2 bd-highlight">
-              {' '}
-              <div class="d-flex flex-row bd-highlight mb-3">
-                <div class="p-2 bd-highlight">
-                  {' '}
-                  <img
-                    src="https://image.flaticon.com/icons/svg/126/126486.svg"
-                    class="sidebar-menu-item-icon"
-                    style={{ width: '15px', height: '15px' }}
-                  />
-                </div>
-                <div class="p-2 bd-highlight">
-                  <span style={{ marginBottom: '10px', marginTop: '10px' }}>
-                    THÔNG TIN KHÁCH HÀNG
-                  </span>
-                </div>
-              </div>
-              <div class="d-flex flex-row bd-highlight mb-3">
-                <div class="p-2 bd-highlight">
-                  <Input placeholder="Tên khách hàng" />
-                </div>
-                <div class="p-2 bd-highlight">
-                  <Input placeholder="Số Điện Thoại" />
-                </div>
-                <div class="p-2 bd-highlight">
-                  <Input.Group compact>
-                    <DatePicker
-                      style={{ width: '100%' }}
-                      hint="Chọn ngày sinh"
-                    />
-                  </Input.Group>
-                </div>
-              </div>
-            </div>
-            <div class="p-2 bd-highlight">
-              <Input placeholder="Địa chỉ Email" />
-            </div>
-          </div>
-        </div>
-        <div
-          className="Address"
-          style={{
-            width: '600px',
-
-            boxSizing: 'border-box',
-            border: '1px solid gray',
-            boxShadow: '0px 0px 0px 1px rgba(0,0,0,0.3)',
-            borderRadius: '5px',
-            float: 'right',
-          }}
-        >
-          <div class="d-flex flex-column bd-highlight mb-3">
+      <div
+        className="Products"
+        style={{
+          width: '658px',
+          boxSizing: 'border-box',
+          border: '1px solid gray',
+          boxShadow: '0px 0px 0px 1px rgba(0,0,0,0.3)',
+          borderRadius: '5px',
+          float: 'left',
+        }}
+      >
+        <div class="d-flex flex-column bd-highlight mb-3">
+          <div class="p-2 bd-highlight">
             <div class="d-flex flex-row bd-highlight mb-3">
               <div class="p-2 bd-highlight">
                 {' '}
                 <img
-                  src="https://image.flaticon.com/icons/svg/157/157933.svg"
+                  src="https://cf.shopee.vn/file/f82f8ccb649afcdf4f07f1dd9c41bcb0"
                   class="sidebar-menu-item-icon"
                   style={{ width: '15px', height: '15px' }}
                 />
               </div>
               <div class="p-2 bd-highlight">
-                <span style={{ marginBottom: '10px' }}>
-                  THÔNG TIN NHẬN HÀNG
-                </span>
+                <span style={{ marginBottom: '10px' }}>QUẢN LÝ ĐƠN HÀNG</span>
               </div>
             </div>
             <div
               class="d-flex flex-row bd-highlight mb-3"
-              style={{ marginLeft: '20px' }}
-            >
-              <div class="p-2 bd-highlight">
-                <img
-                  src="https://image.flaticon.com/icons/svg/845/845998.svg"
-                  class="sidebar-menu-item-icon"
-                  style={{ width: '10px', height: '10px' }}
-                />
-              </div>
-              <div class="p-2 bd-highlight">Ngày dự kiến nhận hàng</div>
-              <div class="p-2 bd-highlight">
-                <DatePicker />
-              </div>
-            </div>
-            <div
-              class="d-flex flex-row bd-highlight mb-3"
-              style={{ marginLeft: '20px' }}
+              style={{ boxSizing: 'border-box' }}
             >
               <div class="p-2 bd-highlight">
                 {' '}
-                <Input placeholder="Tên người nhận" />
+                <InputComponent text={'Tìm sản phẩm'} />
               </div>
               <div class="p-2 bd-highlight">
-                <Input placeholder="Số Điện Thoại" />
-              </div>
-            </div>
-            <div
-              class="d-flex flex-row bd-highlight mb-3"
-              style={{ marginLeft: '20px' }}
-            >
-              <div class="p-2 bd-highlight">
-                {' '}
-                <Input placeholder="Địa chỉ" />
-              </div>
-              <div class="p-2 bd-highlight">
-                <Input placeholder="Quốc gia" />
-              </div>
-            </div>
-            <div
-              class="d-flex flex-row bd-highlight mb-3"
-              style={{ marginLeft: '20px' }}
-            >
-              <div class="p-2 bd-highlight">
-                {' '}
-                <Input placeholder="Thành Phố" />
-              </div>
-              <div class="p-2 bd-highlight">
-                <Input placeholder="Quận Huyện" />
-              </div>
-              <div class="p-2 bd-highlight">
-                <Input placeholder="Phường Xã" />
+                <Button type="primary">Tìm theo QR</Button>
               </div>
             </div>
           </div>
         </div>
+        <TableComponent />
+        <div
+          className="Button-TextArea"
+          style={{ float: 'left', boxSizing: 'border-box', width: '658px' }}
+        >
+          <Button type="primary" style={{ float: 'left' }}>
+            ĐÃ GIAO HÀNG
+          </Button>
+        </div>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+          style={{ float: 'left', boxSizing: 'border-box', width: '658px' }}
+        />
       </div>
+
+      <div
+        className="Customers"
+        style={{
+          width: '600px',
+
+          boxSizing: 'border-box',
+          border: '1px solid gray',
+          boxShadow: '0px 0px 0px 1px rgba(0,0,0,0.3)',
+          borderRadius: '5px',
+          float: 'right',
+        }}
+      >
+        <div class="d-flex flex-column bd-highlight mb-3">
+          <div class="p-2 bd-highlight">
+            {' '}
+            <div class="d-flex flex-row bd-highlight mb-3">
+              <div class="p-2 bd-highlight">
+                {' '}
+                <img
+                  src="https://image.flaticon.com/icons/svg/126/126486.svg"
+                  class="sidebar-menu-item-icon"
+                  style={{ width: '15px', height: '15px' }}
+                />
+              </div>
+
+              <div class="p-2 bd-highlight">
+                <span style={{ marginBottom: '10px', marginTop: '10px' }}>
+                  THÔNG TIN KHÁCH HÀNG
+                </span>
+              </div>
+            </div>
+            <div class="d-flex flex-row bd-highlight mb-3">
+              <div class="p-2 bd-highlight">
+                <Input placeholder="Tên khách hàng" />
+              </div>
+              <div class="p-2 bd-highlight">
+                <Input placeholder="Số Điện Thoại" />
+              </div>
+              <div class="p-2 bd-highlight">
+                <Input.Group compact>
+                  <DatePicker
+                    style={{ width: '100%' }}
+                    hint="Chọn ngày sinh"
+                  />
+                </Input.Group>
+              </div>
+            </div>
+          </div>
+          <div class="p-2 bd-highlight">
+            <Input placeholder="Địa chỉ Email" />
+          </div>
+        </div>
+      </div>
+      <div
+        className="Address"
+        style={{
+          width: '600px',
+
+          boxSizing: 'border-box',
+          border: '1px solid gray',
+          boxShadow: '0px 0px 0px 1px rgba(0,0,0,0.3)',
+          borderRadius: '5px',
+          float: 'right',
+        }}
+      >
+        <div class="d-flex flex-column bd-highlight mb-3">
+          <div class="d-flex flex-row bd-highlight mb-3">
+            <div class="p-2 bd-highlight">
+              {' '}
+              <img
+                src="https://image.flaticon.com/icons/svg/157/157933.svg"
+                class="sidebar-menu-item-icon"
+                style={{ width: '15px', height: '15px' }}
+              />
+            </div>
+            <div class="p-2 bd-highlight">
+              <span style={{ marginBottom: '10px' }}>
+                THÔNG TIN NHẬN HÀNG
+              </span>
+            </div>
+          </div>
+          <div
+            class="d-flex flex-row bd-highlight mb-3"
+            style={{ marginLeft: '20px' }}
+          >
+            <div class="p-2 bd-highlight">
+              <img
+                src="https://image.flaticon.com/icons/svg/845/845998.svg"
+                class="sidebar-menu-item-icon"
+                style={{ width: '10px', height: '10px' }}
+              />
+            </div>
+            <div class="p-2 bd-highlight">Ngày dự kiến nhận hàng</div>
+            <div class="p-2 bd-highlight">
+              <DatePicker />
+            </div>
+          </div>
+          <div
+            class="d-flex flex-row bd-highlight mb-3"
+            style={{ marginLeft: '20px' }}
+          >
+            <div class="p-2 bd-highlight">
+              {' '}
+              <Input placeholder="Tên người nhận" />
+            </div>
+            <div class="p-2 bd-highlight">
+              <Input placeholder="Số Điện Thoại" />
+            </div>
+          </div>
+          <div
+            class="d-flex flex-row bd-highlight mb-3"
+            style={{ marginLeft: '20px' }}
+          >
+            <div class="p-2 bd-highlight">
+              {' '}
+              <Input placeholder="Địa chỉ" />
+            </div>
+            <div class="p-2 bd-highlight">
+              <Input placeholder="Quốc gia" />
+            </div>
+          </div>
+          <div
+            class="d-flex flex-row bd-highlight mb-3"
+            style={{ marginLeft: '20px' }}
+          >
+            <div class="p-2 bd-highlight">
+              {' '}
+              <Input placeholder="Thành Phố" />
+            </div>
+            <div class="p-2 bd-highlight">
+              <Input placeholder="Quận Huyện" />
+            </div>
+            <div class="p-2 bd-highlight">
+              <Input placeholder="Phường Xã" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     );
   }
 }
